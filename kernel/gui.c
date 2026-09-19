8c2756f2580ccc37f250c6c78a67678a57ff6ab5
ThatOneMemeGuyFromYoutube <134012505+ThatOneMemeGuyFromYoutube@users.noreply.github.com>
2026-09-19 09:51:57 -0500

    Add pwd command to terminal

--- kernel/gui.c
+++ kernel/gui.c
@@ -20,7 +20,7 @@ static char terminal_output[TERM_INPUT_MAX + 1];
 static char terminal_last[TERM_INPUT_MAX + 1];
 static const char *items[] = {"Terminal", "Files", "Programs", "About"};
 
-enum { TERM_READY = 0, TERM_HELP, TERM_APPS, TERM_INFO, TERM_VER, TERM_ECHO, TERM_UNKNOWN };
+enum { TERM_READY = 0, TERM_HELP, TERM_APPS, TERM_INFO, TERM_VER, TERM_ECHO, TERM_PWD, TERM_UNKNOWN };
 
 static void cell(uint8_t x,uint8_t y,char c,uint8_t a){if(x<WIDTH&&y<HEIGHT)VGA[(uint16_t)y*WIDTH+x]=((uint16_t)a<<8)|(uint8_t)c;}
 static void fill(uint8_t x,uint8_t y,uint8_t w,uint8_t h,char c,uint8_t a){for(uint8_t r=0;r<h;r++)for(uint8_t col=0;col<w;col++)cell((uint8_t)(x+col),(uint8_t)(y+r),c,a);}
@@ -47,6 +47,7 @@ static void terminal_execute(void){
     else if(text_equals(command,"apps")||text_equals(command,"ls")) terminal_result=TERM_APPS;
     else if(text_equals(command,"info")||text_equals(command,"about")) terminal_result=TERM_INFO;
     else if(text_equals(command,"ver")) terminal_result=TERM_VER;
+    else if(text_equals(command,"pwd")) terminal_result=TERM_PWD;
     else if(text_equals(command,"echo")) terminal_result=TERM_ECHO;
     else if(starts_with(command,"echo ")) {
         command += 5;
@@ -65,12 +66,13 @@ static void terminal_execute(void){
 static void draw_terminal_result(void){
     switch(terminal_result){
         case TERM_HELP:
-            text(22,9,"help clear/cls apps/ls info/about ver echo menu",ATTR_NORMAL);
+            text(22,9,"help clear/cls apps/ls info/about ver pwd echo menu",ATTR_NORMAL);
             text(22,10,"Commands ignore leading spaces.",ATTR_NORMAL);
             text(22,11,"echo accepts repeated spaces before text.",ATTR_NORMAL);
             text(22,12,"!! repeats the previous command.",ATTR_NORMAL);
-            text(22,13,"menu returns to the application launcher.",ATTR_NORMAL);
-            text(22,14,"Type a command and press Enter.",ATTR_NORMAL);
+            text(22,13,"pwd shows the current filesystem path.",ATTR_NORMAL);
+            text(22,14,"menu returns to the application launcher.",ATTR_NORMAL);
+            text(22,15,"Type a command and press Enter.",ATTR_NORMAL);
             break;
         case TERM_APPS:
             text(22,9,"Terminal  Files  Programs  About",ATTR_NORMAL);
@@ -84,6 +86,9 @@ static void draw_terminal_result(void){
             break;
         case TERM_ECHO:
             text_n(22,9,terminal_output,ATTR_NORMAL,TERM_INPUT_MAX);
+            break;
+        case TERM_PWD:
+            text(22,9,"/",ATTR_NORMAL);
             break;
         case TERM_UNKNOWN:
             text(22,9,"Unknown command. Type help.",ATTR_NORMAL);
